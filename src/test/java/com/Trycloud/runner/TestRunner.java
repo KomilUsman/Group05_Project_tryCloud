142714d42a9bda7e933f4947f0da2948c73b7765
Komil <87507641+KomilUsman@users.noreply.github.com>
2021-10-16 12:44:16 -0400

    update

--- src/test/java/com/Trycloud/runner/TestRunner.java
+++ src/test/java/com/Trycloud/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", //"classpath:features"
+@CucumberOptions(features = "src/test/resources/4rrwrwr", //"classpath:4rrwrwr"
         glue = "com/cydeo/step_definitions",
         publish = true,
         plugin = {"pretty", "html:target/cucumber_report.html",

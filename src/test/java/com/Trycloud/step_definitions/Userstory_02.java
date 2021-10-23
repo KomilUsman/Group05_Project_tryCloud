e25dbeb851e6fb311fd6c8236de6b605668adf19
Altan-Ochir <altanochir0130@yahoo.com>
2021-10-23 14:53:10 -0500

    us2

--- src/test/java/com/Trycloud/step_definitions/Userstory_02.java
+++ src/test/java/com/Trycloud/step_definitions/Userstory_02.java
@@ -1,6 +1,39 @@
 package com.Trycloud.step_definitions;
 
+import com.Trycloud.pages.DashboardPage;
+import com.Trycloud.pages.LoginPage;
+import com.Trycloud.utilities.BrowserUtil;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class Userstory_02 {
 
+    LoginPage loginPage = new LoginPage();
+
+    @When("user enter with valid credentials")
+    public void user_enter_with_valid_credentials() {
+
+       loginPage.login("user5");
+    }
+
+    @When("user enter with valid credentials {string} and {string}")
+    public void userEnterWithValidCredentialsAnd(String username, String password) {
+        loginPage.login(username, password);
+    }
+
+    @Then("Verify user see the following modules")
+    public void verify_user_see_the_following_modules(List<String> expectedResult) {
+
+        List<String> actualModules = BrowserUtil.getAllText(new DashboardPage().allModules);
+
+        Assert.assertEquals(expectedResult, actualModules);
+
+    }
 
 }

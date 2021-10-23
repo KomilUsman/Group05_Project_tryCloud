550f48b5388e42e8c23372b01adca866184e46a9
Altan-Ochir <altanochir0130@yahoo.com>
2021-10-23 18:44:55 -0500

    added some change on user story 2

--- src/test/java/com/Trycloud/runner/TestRunner.java
+++ src/test/java/com/Trycloud/runner/TestRunner.java
@@ -6,14 +6,14 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/4rrwrwr", //"classpath:4rrwrwr"
+@CucumberOptions(features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         publish = true,
         plugin = {"pretty", "html:target/cucumber_report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target"},
         dryRun = false
-       // , tags = "@us_02"
+       // , tags = "@wip"
 )
 
 public class TestRunner {

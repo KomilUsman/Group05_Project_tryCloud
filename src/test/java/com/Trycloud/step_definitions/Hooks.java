56a735501b0bdd95a4b204771a146d0c1221aee5
Altan-Ochir <altanochir0130@yahoo.com>
2021-10-24 20:01:23 -0500

    added takesScreenshot method in the Hook class

--- src/test/java/com/Trycloud/step_definitions/Hooks.java
+++ src/test/java/com/Trycloud/step_definitions/Hooks.java
@@ -1,21 +1,35 @@
 package com.Trycloud.step_definitions;
 
+import com.Trycloud.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before
+    @Before()
     public void setupDriver(){
         System.out.println("This is from @Before inside hook class");
         com.Trycloud.utilities.Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
-    @After
-    public void teardown(){
+    @After()
+    public void teardown(Scenario scenario){
+
+        if(scenario.isFailed() ){
+
+            TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", "This is what is it");
+
+        }
+
         System.out.println("This is from @After inside hook class");
         com.Trycloud.utilities.Driver.closeBrowser();
 

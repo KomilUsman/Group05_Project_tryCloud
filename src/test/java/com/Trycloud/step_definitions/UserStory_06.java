ae6a627cbdb1de3a56353daaaf5a332e5ffc8d2a
Malika1201 <malika.lrjj@yahoo.com>
2021-11-03 11:29:23 -0500

    test

---
+++ src/test/java/com/Trycloud/step_definitions/UserStory_06.java
@@ -0,0 +1,16 @@
+package com.Trycloud.step_definitions;
+
+public class UserStory_06 {
+    public UserStory_06() {
+        Then("^user clicks the “\\+” icon on top$", () -> {
+        });
+        Then("^user clicks “New Folder”$", () -> {
+        });
+        Then("^user creates a folder name$", () -> {
+        });
+        Then("^user clicks “Submit“ icon$", () -> {
+        });
+        Then("^the folder is displayed on the page$", () -> {
+        });
+    }
+}

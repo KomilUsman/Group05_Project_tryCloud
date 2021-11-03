c6a9122d5e7b41049803d592456beb5a6b4c4f57
Matthew K <wabasi333@gmail.com>
2021-11-03 11:34:57 -0500

    aiding US6

---
+++ src/test/java/com/Trycloud/step_definitions/Userstory_06.java
@@ -0,0 +1,25 @@
+package com.Trycloud.step_definitions;
+
+import io.cucumber.java.en.Then;
+
+public class Userstory_06 {
+    @Then("user clicks the “+” icon on top")
+    public void userClicksTheIconOnTop() {
+    }
+
+    @Then("user clicks “New Folder”")
+    public void userClicksNewFolder() {
+    }
+
+    @Then("user creates a folder name")
+    public void userCreatesAFolderName() {
+    }
+
+    @Then("user clicks “Submit“ icon")
+    public void userClicksSubmitIcon() {
+    }
+
+    @Then("the folder is displayed on the page")
+    public void theFolderIsDisplayedOnThePage() {
+    }
+}
